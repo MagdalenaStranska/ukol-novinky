3145e3cb753d589b32fd389a01e4f53abd6b1ce6
MagdalenaStranska <stranska.magdalena@gmail.com>
2024-10-21 19:35:19 +0200

    Pridavam reseni

---
+++ index.js
@@ -0,0 +1,19 @@
+const bodyElement = document.querySelector("body")
+
+bodyElement.style.backgroundColor = "#e9e9e9"
+
+const novinky = document.querySelector(".news")
+
+novinky.style.backgroundColor = "white"
+novinky.style.width = "60rem"
+
+const nadpis = document.querySelector("h1")
+nadpis.classList.add("news__title")
+nadpis.textContent = "Aktualni novinky"
+
+const zprava1 = document.querySelector("#zprava1")
+zprava1.classList.add("post--main")
+
+const zprava3 = document.querySelector("#zprava3 img")
+zprava3.src = "images/zprava3-novy.jpg"
+console.log(zprava3)
