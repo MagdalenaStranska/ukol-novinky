d7c57c869b910a4f08440b739a65f235d5418b92
MagdalenaStranska <stranska.magdalena@gmail.com>
2024-11-04 16:57:36 +0100

    Pridavam reseni lektora

--- index.js
+++ index.js
@@ -26,3 +26,27 @@ zprava1.classList.add("post--main")
 const zprava3 = document.querySelector("#zprava3 img")
 zprava3.src = "images/zprava3-novy.jpg"
 console.log(zprava3)
+
+
+// Reseni dle lektora:
+// // a.
+// const bodyElement = document.querySelector('body');
+// bodyElement.style.backgroundColor = '#e9e9e9';
+
+// // b.
+// const newsElm = document.querySelector('.news');
+// newsElm.style.backgroundColor = 'white';
+// newsElm.style.maxWidth = '60rem';
+
+// // c.
+// const headingElm = document.querySelector('h1');
+// headingElm.classList.add('news__title');
+// headingElm.textContent = 'Aktuální novinky';
+
+// // d.
+// const firstPostElm = document.getElementById('zprava1');
+// firstPostElm.classList.add('post--main');
+
+// // e.
+// const lastPostImgElm = document.querySelector('#zprava3 img');
+// lastPostImgElm.src = 'images/zprava3-novy.jpg';
