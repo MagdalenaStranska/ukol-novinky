fc2a95352408a90b8518fc82e44f8559c64e168a
MagdalenaStranska <stranska.magdalena@gmail.com>
2024-11-04 16:55:49 +0100

    Pridavam reseni vcetne zadani

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
+// Vytvořte si repozitář ze šablony cviceni-novinky. V repozitáři je připravena webová stránka, která zobrazuje několik zajímavých zpráv. Zobrazte stránku v prohlížeči a veškeré úkoly z tohoto cvičení proveďte v JavaScriptovém souboru index.js.
+
+// 1. Pomocí document.querySelector vyberte element body a uložte si jej do proměnné bodyElement. Pomocí této proměnné nastavte barvu pozadí elementu na hodnotu #e9e9e9.
+// 2. Do jiné proměnné vyberte element s třídou news a nastavte mu barvu pozadí na bílou a maximální šířku na 60rem.
+// 3. Vyberte element h1 a nastavte mu (v JavaScriptu) třídu na news__title. Nadpis by měl změnit styl. Nastavte také obsah nadpisu na text Aktuální novinky.
+// 4. Pomocí atributu id vyberte element obsahující první zprávu. Přidejte do jeho atributu class třídu post--main. První zpráva by tak měla mírně změnit svůj styl.
+// 5. Pomocí CSS selektoru vyberte obrázek ve třetí zprávě (id zprávy je zprava3) a změňte jeho atribut src na obrázek images/zprava3-novy.jpg.
+// Na konci by stránka v prohlížeči měla vypadat jako na obrázku níže:
+
 const bodyElement = document.querySelector("body")
 
 bodyElement.style.backgroundColor = "#e9e9e9"
